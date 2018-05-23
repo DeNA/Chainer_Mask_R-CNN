9433b4a8fd6856626d000761ad16cf9d4a3c244d
hirotomusiker <hirotoschwert@gmail.com>
2018-05-24 00:16:05 +0900

    add class_ids

--- demo.py
+++ demo.py
@@ -14,6 +14,12 @@ def main():
     parser.add_argument('--extractor', choices=('resnet50','resnet101'),
                         default='resnet50', help='extractor network')
     args = parser.parse_args()
+
+    #network class id --> coco label id
+    test_class_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, \
+    27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, \
+    57, 58, 59, 60, 61, 62, 63, 64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
+
     if args.background:
         import matplotlib
         matplotlib.use('Agg')
@@ -23,9 +29,10 @@ def main():
     from mask_rcnn_resnet import MaskRCNNResNet
     from chainercv import utils
     if args.extractor=='resnet50':
-        model = MaskRCNNResNet(n_fg_class=80, roi_size=args.roi_size, n_layers=50, roi_align=args.roialign)
+        model = MaskRCNNResNet(n_fg_class=80, roi_size=args.roi_size, pretrained_model='auto', n_layers=50, roi_align=args.roialign, class_ids=test_class_ids)
     elif args.extractor=='resnet101':
-        model = MaskRCNNResNet(n_fg_class=80, roi_size=args.roi_size, n_layers=101, roi_align=args.roialign)
+        model = MaskRCNNResNet(n_fg_class=80, roi_size=args.roi_size, pretrained_model='auto', n_layers=101, roi_align=args.roialign, class_ids=test_class_ids)
+
     chainer.serializers.load_npz(args.modelfile, model)
     if args.gpu >= 0:
         chainer.cuda.get_device_from_id(args.gpu).use()
@@ -48,7 +55,7 @@ def main():
 
     )
     vis_bbox(
-        img, roi, roi, label=label, score=score, mask=mask, label_names=coco_label_names, contour=args.contour, labeldisplay=True)
+        img, bbox, roi, label=label, score=score, mask=mask, label_names=coco_label_names, contour=args.contour, labeldisplay=True)
     plot.show()
     filename = "output.png"
     plot.savefig(filename)
