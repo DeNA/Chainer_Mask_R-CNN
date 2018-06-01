e8e0024fb0897b3aa99fc5c34fea58432bb329a6
hirotomusiker <hirotoschwert@gmail.com>
2018-06-02 00:51:09 +0900

    modify roialign

--- utils/roi_align_2d.py
+++ utils/roi_align_2d.py
@@ -64,7 +64,7 @@ class ROIAlign2D(function.Function):
             int32 height, int32 width, int32 pooled_height, int32 pooled_width,
             raw float32 bottom_rois
             ''',
-            'float32 top_data, int32 argmax_data',
+            'float32 top_data',
             '''
             // pos in output filter
             int pw = i % pooled_width;
@@ -90,41 +90,53 @@ class ROIAlign2D(function.Function):
             int maxidx = -1;
             
             for (int j = 0; j < 4; j++) {
-                int i = j / 2;
+                int ih = j / 2;
+                int iw = j % 2;
                 float val = 0;
                 // ROIAlign using the center of the bin
-                int hstart = static_cast<int>(floor((static_cast<float>(ph) + 0.25 + i * 0.5)
-                                              * bin_size_h));
-                int wstart = static_cast<int>(floor((static_cast<float>(pw) + 0.25 + (j % 2) * 0.5)
-                                              * bin_size_w));
-                int hend = static_cast<int>(ceil((static_cast<float>(ph) + 0.25 + i * 0.5)
-                                            * bin_size_h)+1);
-                int wend = static_cast<int>(ceil((static_cast<float>(pw) + 0.25 + (j % 2) * 0.5)
-                                            * bin_size_w)+1);
-                hstart = min(max(hstart + roi_start_h, 0.0), static_cast<float>(height));
-                hend = min(max(hend + roi_start_h, 0.0),static_cast<float>(height));
-                wstart = min(max(wstart + roi_start_w, 0.0), static_cast<float>(width));
-                wend = min(max(wend + roi_start_w, 0.0), static_cast<float>(width));
+                float fh = roi_start_h + (static_cast<float>(ph) + 0.25 + static_cast<float>(ih) * 0.5f) * bin_size_h;
+                float fw = roi_start_w + (static_cast<float>(pw) + 0.25 + static_cast<float>(iw) * 0.5f) * bin_size_w;
+                
+                if (fh < -1.0 || fh > height || fw < -1.0 || fw > width) {
+                    continue;
+                }
+
+                int hstart = static_cast<int>(floor(fh));
+                int wstart = static_cast<int>(floor(fw));
+                int hend = hstart + 1;
+                int wend = wstart + 1;
+
+                if (hstart >= height - 1) {
+                    hend = hstart = height - 1;
+                    fh = static_cast<float>(hstart);
+                } else {
+                    hend = hstart + 1;
+                }
+
+                if (wstart >= width - 1) {
+                    wend = wstart = width - 1;
+                    fw = static_cast<float>(wstart);
+                } else {
+                    wend = wstart + 1;
+                }
+                float dh = fh - static_cast<float>(hstart);
+                float dw = fw - static_cast<float>(wstart);
+
                 //compute the max value in the bin
                 int data_offset = (roi_batch_ind * channels + c) * height * width;
-                for (int h = hstart; h < hend; ++h) {
-                    for (int w = wstart; w < wend; ++w) {
-                        int bottom_index = h * width + w;
-                        val += bottom_data[data_offset + bottom_index];
-                    }
-                }
-                if (maxval < val) {
-                    maxval = val;
-                    maxidx = hstart * width + wstart; 
-                }
+
+                val += (1.0 - dh) * (1.0 - dw) * bottom_data[data_offset + hstart * width + wstart];
+                val += (1.0 - dh) * dw         * bottom_data[data_offset + hstart * width + wend];
+                val += dh * (1.0 - dw)         * bottom_data[data_offset + hend * width + wstart];
+                val += dh * dw                 * bottom_data[data_offset + hend * width + wend];
+
+                maxval += val;
             }
             top_data = maxval / 4;
-            argmax_data = maxidx;
             
             ''', 'roi_pooling_2d_fwd'
         )(bottom_data, self.spatial_scale, channels, height, width,
-          self.outh, self.outw, bottom_rois, top_data,
-          self.argmax_data)
+          self.outh, self.outw, bottom_rois, top_data)
         return top_data,
 
     def backward_gpu(self, inputs, gy):
@@ -137,78 +149,73 @@ class ROIAlign2D(function.Function):
             float32 spatial_scale, int32 channels, int32 height, int32 width,
             int32 pooled_height, int32 pooled_width, raw float32 bottom_rois
             ''',
-            'float32 bottom_diff',
+            'raw float32 bottom_diff',
             '''
-            int w = i % width;
-            int h = (i / width) % height;
-            int c = (i / (width * height)) % channels;
-            int num = i / (width * height * channels);
+            // pos in output filter
+            int pw = i % pooled_width;
+            int ph = (i / pooled_width) % pooled_height;
+            int c = (i / pooled_width / pooled_height) % channels;
+            int num = i / pooled_width / pooled_height / channels;
 
-            float gradient = 0;
-            // Accumulate gradient over all ROIs that pooled this element
-            for (int roi_n = 0; roi_n < num_rois; ++roi_n) {
-                // Skip if ROI's batch index doesn't match num
-                if (num != static_cast<int>(bottom_rois[roi_n * 5])) {
-                    continue;
-                }
-                // same as forward
-                float roi_start_w = bottom_rois[roi_n * 5 + 1] * spatial_scale;
-                float roi_start_h = bottom_rois[roi_n * 5 + 2] * spatial_scale;
-                float roi_end_w = bottom_rois[roi_n * 5 + 3] * spatial_scale;
-                float roi_end_h = bottom_rois[roi_n * 5 + 4] * spatial_scale;
+            // scale the ROI coordinates (1/16)
+            float roi_batch_ind = bottom_rois[num * 5 + 0];
+            float roi_start_w = bottom_rois[num * 5 + 1] * spatial_scale;
+            float roi_start_h = bottom_rois[num * 5 + 2] * spatial_scale;
+            float roi_end_w = bottom_rois[num * 5 + 3] * spatial_scale;
+            float roi_end_h = bottom_rois[num * 5 + 4] * spatial_scale;
 
-                // Skip if ROI doesn't include (h, w)
-                const bool in_roi = (w >= roi_start_w && w <= roi_end_w &&
-                                     h >= roi_start_h && h <= roi_end_h);
-                if (!in_roi) {
-                    continue;
-                }
+            // Force malformed ROIs to be 1x1
+            float roi_width = max(roi_end_w - roi_start_w, 1.0);
+            float roi_height = max(roi_end_h - roi_start_h, 1.0);
 
-                int offset = (roi_n * channels + c) * pooled_height * pooled_width;
-
-                // Compute feasible set of pooled units that could have pooled
-                // this bottom unit
-
-                // Force malformed ROIs to be 1x1
-                float roi_width = max(roi_end_w - roi_start_w, 1.0);
-                float roi_height = max(roi_end_h - roi_start_h, 1.0);
-
-                float bin_size_h = static_cast<float>(roi_height)
-                               / static_cast<float>(pooled_height);
-                float bin_size_w = static_cast<float>(roi_width)
-                               / static_cast<float>(pooled_width);
+            // float bin size 
+            float bin_size_h = roi_height / static_cast<float>(pooled_height);
+            float bin_size_w = roi_width / static_cast<float>(pooled_width);
+            int data_offset = (roi_batch_ind * channels + c) * height * width;
+            
+            for (int j = 0; j < 4; j++) {
+                int ih = j / 2;
+                int iw = j % 2;
                 // ROIAlign using the center of the bin
-                int phstart = floor(static_cast<float>(h - roi_start_h)
-                                    / bin_size_h);
-                int phend = ceil(static_cast<float>(h - roi_start_h + 1)
-                                 / bin_size_h);
-                int pwstart = floor(static_cast<float>(w - roi_start_w)
-                                    / bin_size_w);
-                int pwend = ceil(static_cast<float>(w - roi_start_w + 1)
-                                 / bin_size_w);
-
-                phstart = min(max(phstart, 0), pooled_height);
-                phend = min(max(phend, 0), pooled_height);
-                pwstart = min(max(pwstart, 0), pooled_width);
-                pwend = min(max(pwend, 0), pooled_width);
-    
-                for (int ph = phstart; ph < phend; ++ph) {
-                    for (int pw = pwstart; pw < pwend; ++pw) {
-                        int index_ = ph * pooled_width + pw + offset;
-                        if (argmax_data[index_] == (h * width + w) ||
-                        argmax_data[index_] == (h * width + w - 1) ||
-                        argmax_data[index_] == ((h - 1) * width + w) ||
-                        argmax_data[index_] == ((h - 1) * width + w - 1)) {
-                            gradient += top_diff[index_];
-                        }
-                    }
+                float fh = roi_start_h + (static_cast<float>(ph) + 0.25 + static_cast<float>(ih) * 0.5f) * bin_size_h;
+                float fw = roi_start_w + (static_cast<float>(pw) + 0.25 + static_cast<float>(iw) * 0.5f) * bin_size_w;
+                
+                if (fh < -1.0 || fh > height || fw < -1.0 || fw > width) {
+                    continue;
                 }
+
+                int hstart = static_cast<int>(floor(fh));
+                int wstart = static_cast<int>(floor(fw));
+                int hend = hstart + 1;
+                int wend = wstart + 1;
+
+                if (hstart >= height - 1) {
+                    hend = hstart = height - 1;
+                    fh = static_cast<float>(hstart);
+                } else {
+                    hend = hstart + 1;
+                }
+
+                if (wstart >= width - 1) {
+                    wend = wstart = width - 1;
+                    fw = static_cast<float>(wstart);
+                } else {
+                    wend = wstart + 1;
+                }
+                float dh = fh - static_cast<float>(hstart);
+                float dw = fw - static_cast<float>(wstart);
+
+                //atomic add: pointer, value
+                atomicAdd(&bottom_diff[data_offset + hstart * width + wstart], top_diff[i] * (1.0 - dh) * (1.0 - dw) / 4);
+                atomicAdd(&bottom_diff[data_offset + hstart * width + wend], top_diff[i] * (1.0 - dh) * dw         / 4);
+                atomicAdd(&bottom_diff[data_offset + hend * width + wstart], top_diff[i] * dh         * (1.0 - dw) / 4);
+                atomicAdd(&bottom_diff[data_offset + hend * width + wend], top_diff[i] * dh         * dw         / 4);
             }
-            bottom_diff = gradient / 4;
+
             ''', 'roi_pooling_2d_bwd'
         )(gy[0], self.argmax_data, bottom_rois.shape[0], self.spatial_scale,
           channels, height, width, self.outh, self.outw,
-          bottom_rois, bottom_diff)
+          bottom_rois, bottom_diff, size=gy[0].size)
         
         #bottom_diff.shape : e.g. (1,512, 38,50)
 
